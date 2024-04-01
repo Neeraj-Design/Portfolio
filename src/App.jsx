51b11fcb45dca83eb71260fa7f19fd63705dcfee
NidhishR7 <codenidhish07@gmail.com>
2024-04-01 17:37:19 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/no-unescaped-entities */
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+      <div className="main">
+        <div className="hersection">
+        <p className='hello'>Hello, I'm</p>
+        <p className='neeraj'>Neeraj Kumar.</p>
+        <p className='designer'>Visual & Experience Designer</p>
+        </div>
+        <div className="hrdiv">
+
+       <hr className='hr-line ' />
+        </div>
+        <button className='continue'>Click To Continue</button>
+      </div>
+   </>
+  )
+}
+
+export default App
