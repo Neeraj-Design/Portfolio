9d1d2bc5a27c46faabcfa4b6f74f17b343c3c7bc
NidhishR7 <codenidhish07@gmail.com>
2024-05-10 01:06:00 +0530

    about fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,9 @@
-import Navbar from '../components/Navbar'; // Import your Navbar component
+import Navbar from '../components/Navbar';
 import './About.css'
 const About = () => {
   return (
     <>
-      <Navbar /> {/* Render the Navbar component */}
+      <Navbar /> 
      <div className="heroSection">
       <div className="leftSide">
         <img src="/Portfolio/DP.png" alt="" />
@@ -38,6 +38,22 @@ const About = () => {
       </div>
      </div>
      <div className="horizLine1"></div>
+
+     <div className="foot-container">
+      <div className="resume-section">
+        <span>I’m thrilled to answer to</span>
+        <span>your next project <img src="/Portfolio/arrow.png" alt="" /></span>
+        <button className='viewResumebtn'>View Resume</button>
+      </div>
+      <div className="contact-section">
+        <p>neeraj942000@gmail.com</p>
+        <div className="socials">
+          <img src="/Portfolio/linkedin.png" alt="" />
+          <img src="/Portfolio/instagram.png" alt="" />
+        </div>
+      </div>
+      </div>
+      <p className='neeraj-foot'>Neeraj Kumar -2024</p>
     </>
   );
 };
