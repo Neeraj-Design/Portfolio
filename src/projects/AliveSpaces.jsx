3dc56abb12ea8db27a4181b3f8e617847ee13ad7
NidhishR7 <codenidhish07@gmail.com>
2024-05-11 19:49:39 +0530

    p3 video

--- src/projects/AliveSpaces.jsx
+++ src/projects/AliveSpaces.jsx
@@ -1,15 +1,16 @@
 import Navbar from '../components/Navbar';
-import './project.css'
+import './project.css';
 
 const AliveSpaces = () => {
   return (
     <>
-    <Navbar /> 
-     <div className="project3-container">
-      <img src="/Portfolio/p31.png" alt="" />
-    </div>
+      <Navbar /> 
+      <div className="project3-container">
+        <img src="/Portfolio/p31.png" alt="" />
+        <video src="/Portfolio/Mockup.mp4" autoPlay loop controls={false}></video>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AliveSpaces
+export default AliveSpaces;
