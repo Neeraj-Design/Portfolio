7e053f548a12e7255f116d7b6cf30d8be9c923a3
NidhishR7 <codenidhish07@gmail.com>
2024-05-11 20:13:10 +0530

    p3 completed

--- src/projects/AliveSpaces.jsx
+++ src/projects/AliveSpaces.jsx
@@ -8,6 +8,7 @@ const AliveSpaces = () => {
       <div className="project3-container">
         <img src="/Portfolio/p31.png" alt="" />
         <video src="/Portfolio/Mockup.mp4" autoPlay loop controls={false} playsInline></video>
+        <img src="/Portfolio/p3.jpg" alt="" />
       </div>
     </>
   );
