4b3ebb7db1d6c17db5a94736507dbd24a609862a
NidhishR7 <codenidhish07@gmail.com>
2024-05-15 18:32:12 +0530

    fixes for MODA MUSE

--- src/projects/ModaMuse.jsx
+++ src/projects/ModaMuse.jsx
@@ -1,20 +1,59 @@
+import { useRef, useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
-import './project.css'
+import './project.css';
 
 const ModaMuse = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseDown = (event) => {
+      setIsDragging(true);
+      setStartX(event.pageX - scrollRef.current.offsetLeft);
+      setScrollLeft(scrollRef.current.scrollLeft);
+    };
+
+    const handleMouseUp = () => {
+      setIsDragging(false);
+    };
+
+    const handleMouseMove = (event) => {
+      if (!isDragging) return;
+      event.preventDefault();
+      const x = event.pageX - scrollRef.current.offsetLeft;
+      const walk = (x - startX) * 2; // Adjust the multiplier to control the drag speed
+      scrollRef.current.scrollLeft = scrollLeft - walk;
+    };
+
+    const scrollContainer = scrollRef.current;
+
+    scrollContainer.addEventListener('mousedown', handleMouseDown);
+    document.addEventListener('mouseup', handleMouseUp);
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      scrollContainer.removeEventListener('mousedown', handleMouseDown);
+      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [isDragging, scrollLeft, startX]);
+
   return (
     <>
-    <Navbar /> 
-    <div className="project1-container">
-      <img src="/Portfolio/p11.jpg" alt="" />
-  
-    </div>
-    <div className="proj1-scrolling">
-
-      <img className='scrollingimg' src="/Portfolio/p12.jpg" alt="" />
-    </div>
+      <Navbar /> 
+      <div className="project1-container">
+        <img src="/Portfolio/p11.jpg" alt="" />
+      </div>
+      <div className="proj1-scrolling" ref={scrollRef}>
+        <img className='scrollingimg' src="/Portfolio/p12.jpg" alt="" />
+      </div>
+      <div className="project1-container">
+        <img src="/Portfolio/p13.jpg" alt="" />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ModaMuse
+export default ModaMuse;
